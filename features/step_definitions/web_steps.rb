05c1126de9492e64a16bf5bb460793cb19e49087
bestofwon <ddalpang22@naver.com>
2013-03-03 15:10:24 +0900

    Creation of projects feature complete

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,20 @@
+Given(/^(?:|I )am on the homepage$/) do 
+	visit root_url
+end
+
+When(/^I follow "New Project"$/) do 
+	 page.should have_link('New Project', href: new_project_path)
+	 visit new_project_path  
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |name, text|
+	fill_in name, with: text
+end
+
+When(/^I press "(.*?)"$/) do |button|
+	click_button button
+end
+
+Then(/^I should see "(.*?)"$/) do |notice|
+	page.should have_content(notice)
+end
